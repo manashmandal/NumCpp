f93973ed559c65e36b8a3e619e4e8efb7aedf9f9
Manash Mandal <manashmndl@gmail.com>
2015-11-28 01:46:29 +0600

    size shape fixed for two dimensional array

--- NumCPP/NumCplusplus/array2D.hpp
+++ NumCPP/NumCplusplus/array2D.hpp
@@ -24,23 +24,53 @@ namespace numcpp{
 		//Printing subroutine
 		void print(short dimension = 2);
 		
+		//SizeShape
+		struct sizeShape {
+		public:
+			size_t row;
+			size_t column;
+			size_t height;
+			
+			//Overloaded operator for sizeShape
+			friend std::ostream& operator<<(std::ostream& os, sizeShape &s){
+				os << "Row: " << s.row << endl
+			   << "Column: " << s.column << endl
+			   << "Height: " << s.height << endl;
+				return os;
+			}
+		};
+		
+		sizeShape shape(void) const;
+		
+		
 	private:
 		oneDimensionalArray var1D;
 		twoDimensionalArray var2D;
 		threeDimensionalArray var3D;
 		
+		sizeShape _shape;
+		
 	};
 	
 	template <class T> array2D<T>::array2D(twoDimensionalArray &arr){
 		var2D = arr;
+		_shape.row = var2D.size();
+		_shape.column = var2D[0].size();
+		_shape.height = 0;
 	}
 	
 	template <class T> array2D<T>::array2D(oneDimensionalArray &arr){
 		var1D = arr;
+		_shape.row = 1;
+		_shape.column = var1D.size();
+		_shape.height = 0;
 	}
 	
 	template <class T> array2D<T>::array2D(threeDimensionalArray &arr){
 		var3D = arr;
+		_shape.row = var3D.size();
+		_shape.column = var3D[0].size();
+		_shape.height = var3D[0][0].size();
 	}
 	
 	template <class T> void array2D<T>::print(short dimension){
@@ -63,6 +93,10 @@ namespace numcpp{
 				cout << "Dimensions only can be 1, 2 and 3" << endl;
 				break;
 		}
+	}
+	
+	template <class T> typename array2D<T>::sizeShape array2D<T>::shape(void) const {
+		return _shape;
 	}
 }
 
--- NumCPP/NumCplusplus/main.cpp
+++ NumCPP/NumCplusplus/main.cpp
@@ -8,7 +8,13 @@ using namespace numcpp;
 
 int main(void){
 	auto x = NumCpp<int>::array({1, 2, 3 ,4 ,5 ,6});
-	array1D<int>::sizeShape _s = x.shape();
-	cout << _s << endl;
+	auto b = NumCpp<int>::array({{1, 2, 3},{2, 4, 5}});
 	
+	auto c = x.reshape(2, 3);
+	
+	c.print();
+	auto size = c.shape();
+	
+	cout << size << endl;
+	return 0;
 }
